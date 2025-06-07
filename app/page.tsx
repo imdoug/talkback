2f334ccb8b7fe07faee1eb4dcb03213bd5af21a4
Douglas Moreira <imdoug@icloud.com>
2025-06-07 17:48:45 -0400

    base

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+        <h1 className='text-2xl underline'> Welcome to my sass app</h1>
+        <Button>  Let&apos;s get started </Button>
+    </>
+  )
+}
+
+export default Page
