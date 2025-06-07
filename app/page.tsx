780b309c1857d38dd1f98ec06f95c0b35d7502a8
Douglas Moreira <imdoug@icloud.com>
2025-06-07 19:01:21 -0400

    CompanionCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <>
-        <h1 className='text-2xl underline'> Welcome to my sass app</h1>
-        <Button>  
-            Let&apos;s get started 
-        </Button>
-    </>
-  )
-}
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizzard"
+          topic="Derivatives % Integrals"
+          subject="maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabuary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
